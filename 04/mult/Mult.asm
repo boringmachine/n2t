fbd5c84e1d9b36acbf23a7f2d62dd4342e46c0dd
boringmachine <flank1990@mail.ru>
2015-05-07 12:54:50 +0900

    update

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	
+	@R2
+	M=0
+
+	@R1
+	D=M
+
+	@count
+	M=D+1
+	
+(LOOP)
+	@count
+	M=M-1
+
+	@count
+	D=M
+	
+	@END
+	D;JEQ
+
+	@R0
+	D=M
+
+	@R2
+	M=M+D
+
+	@LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
