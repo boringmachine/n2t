26ded08be376e7b7d7dc77bb117453a21d913465
boringmachine <flank1990@mail.ru>
2015-05-14 12:03:54 +0900

    test

---
+++ nand2tetris/Nand2Tetris/parse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys, binascii
+infile = "aaa.hack"
+outfile = "ddd.hack"
+
+f = open(infile,"r")
+g = open(outfile,"w+")
+code = f.read()
+code2 = binascii.b2a_hex(code)
+
+def split_str(s, n):
+    length = len(s)
+    return [s[i:i+n] for i in range(0, length, n)]
+
+parsed = bin(int(code2, 16))[2:].zfill(16)
+parsed2 = split_str(parsed, 16)
+for p in parsed2:
+    g.write(p+"\n")
+    
+g.close()
+
+
