f3601d0bab6ac684b0b8c2f28157250ee03656ed
boringmachine <flank1990@mail.ru>
2015-05-28 12:28:22 +0900

    fix VM

--- nand2tetris/Nand2Tetris/VMtranslator.sh
+++ nand2tetris/Nand2Tetris/VMtranslator.sh
@@ -1,2 +1,4 @@
 #/bin/bash
 java -classpath bin VMtranslator.VMtranslator $1
+file=${1%.vm}
+./assembly.sh $file
--- nand2tetris/Nand2Tetris/assembly.sh
+++ nand2tetris/Nand2Tetris/assembly.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-java -classpath bin Assembler.Assembler Asm/$1.asm
-./bin2ascii.py Hack/$1.hack
+java -classpath bin Assembler.Assembler $1.asm
+./bin2ascii.py $1.hack
